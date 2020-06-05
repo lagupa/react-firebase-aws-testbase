aa88b944a7b2ea04c93df302b89ba57119406a8c
Lagu Stephen <lagustephen@Lagus-MacBook-Pro.local>
2020-06-05 04:21:05 -0500

    implemented auth switch

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,11 +1,22 @@
 import React, { Component } from "react";
 
 export default class Auth extends Component {
+  state = {
+    open: false,
+  };
+  authSwitchLinks = (e) => {
+    console.log("switching");
+    this.setState({
+      open: !this.state.open,
+    });
+  };
   render() {
+    const { open } = this.state;
+    console.log(open);
     return (
       <div className="auth open">
         {/* auth modals */}
-        <div className="modal">
+        <div className={open ? "modal" : "modal active"}>
           <h2>Login</h2>
           <form className="login">
             <input type="text" name="email" placeholder="Email" />
@@ -14,10 +25,14 @@ export default class Auth extends Component {
             <p className="error"></p>
           </form>
           <div>
-            No account? <a className="switch">Register instead</a>
+            No account?
+            <a onClick={this.authSwitchLinks} className="switch">
+              Register instead
+            </a>
           </div>
         </div>
-        <div className="modal">
+
+        <div className={open ? "modal active" : "modal"}>
           <h2>Register</h2>
           <form className="register">
             <input type="text" name="email" placeholder="Email" />
@@ -26,7 +41,10 @@ export default class Auth extends Component {
             <p className="error"></p>
           </form>
           <div>
-            Got an account? <a className="switch">Login instead</a>
+            Got an account?
+            <a onClick={this.authSwitchLinks} className="switch">
+              Login instead
+            </a>
           </div>
         </div>
       </div>
