301f631d33264426c895859b50ab2b4973d1c087
Lagu Stephen <lagustephen@Lagus-MacBook-Pro.local>
2020-06-06 00:21:22 -0500

    Rendering data from firebase database

--- src/components/ListTutorial.js
+++ src/components/ListTutorial.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
+import firebase from "../firebase/config";
 
 export class ListTutorial extends Component {
+  state = {
+    tutorialsRequests: [],
+  };
+  componentDidMount() {
+    this.getTutorialRequest();
+  }
+
+  // Get tutorial request
+  getTutorialRequest = () => {
+    const ref = firebase
+      .firestore()
+      .collection("requests")
+      .orderBy("upvotes", "desc");
+
+    ref.onSnapshot((snapshot) => {
+      let requests = [];
+      snapshot.forEach((doc) => {
+        requests.push({ ...doc.data(), id: doc.id });
+      });
+
+      this.setState({ tutorialsRequests: requests });
+    });
+  };
+
+  // vote
+  upvoteRequest = (id) => {
+    //console.log(id);
+    const upvote = firebase.functions().httpsCallable("upvote");
+    upvote({ id }).catch((error) => {
+      console.log(error);
+      // showNotification(error.message);
+    });
+  };
   render() {
+    const { tutorialsRequests } = this.state;
     return (
       <section className="content" id="app">
         <h1>Tutorial Requests</h1>
         <ul className="request-list">
-          <li v-for="request in requests">
-            <span className="text"> request.text</span>
-            <div>
-              <span className="votes">request.upvotes</span>
-              <i className="material-icons upvote">arrow_upward</i>
-            </div>
-          </li>
+          {tutorialsRequests.map((tut, i) => (
+            <li key={i}>
+              <span className="text">{tut.text}</span>
+              <div>
+                <span className="votes">{tut.upvotes}</span>
+                <i className="material-icons upvote">arrow_upward</i>
+              </div>
+            </li>
+          ))}
         </ul>
       </section>
     );
